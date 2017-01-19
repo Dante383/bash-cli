1e86f4d99577378968b315b3b997be2acfa9e52a
Dante <piotrfilmy@wp.pl>
2017-01-19 11:45:36 +0100

    Initial commit

---
+++ bash-cli.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+import sys
+import urllib
+from bs4 import BeautifulSoup
+
+if len(sys.argv) == 1:
+    print "Usage: ./bash-cli.py <bash paste id>"
+    exit();
+url = "http://bash.org.pl/" + sys.argv[1]
+f = urllib.urlopen(url)
+html = f.read()
+soup = BeautifulSoup(html, "lxml")
+quote = soup.find("div", class_="quote post-content post-body")
+reset = 0
+if not hasattr(quote, 'contents'):
+    print "Quote not found"
+    exit();
+
+for x in quote.contents:
+    if reset == 1:
+        reset = 0
+        continue
+    reset = 1
+    print x.strip()
+
