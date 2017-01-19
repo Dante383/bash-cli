4590c476b221f8e1a040dd29dcb902e20932aa20
Dante <piotrfilmy@wp.pl>
2017-01-19 13:33:24 +0100

    Update bash-cli.py
    Now can choose random quote!

--- bash-cli.py
+++ bash-cli.py
@@ -5,16 +5,21 @@ import urllib
 from bs4 import BeautifulSoup
 
 if len(sys.argv) == 1:
-    print "Usage: ./bash-cli.py <bash paste id>"
+    print "Usage: ./bash-cli.py <bash paste id or -r for random>"
     exit();
-url = "http://bash.org.pl/" + sys.argv[1]
+
+if sys.argv[1] == "-r":
+    url = "http://bash.org.pl/random"
+else:
+    url = "http://bash.org.pl/" + sys.argv[1]
+
 f = urllib.urlopen(url)
 html = f.read()
 soup = BeautifulSoup(html, "lxml")
 quote = soup.find("div", class_="quote post-content post-body")
 reset = 0
 if not hasattr(quote, 'contents'):
-    print "Quote not found"
+    print "Quote not found, use -r instead of quote id to choose random quote"
     exit();
 
 for x in quote.contents:
@@ -23,4 +28,3 @@ for x in quote.contents:
         continue
     reset = 1
     print x.strip()
-
